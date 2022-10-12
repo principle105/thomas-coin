cad56a624cd811ed7d843f6d4633e5fd9a357b48
principle105 <principle105@gmail.com>
2022-10-11 21:09:38 -0400

    more efficient scheduler

--- tcoin/p2p/nodes/scheduler.py
+++ tcoin/p2p/nodes/scheduler.py
@@ -20,6 +20,8 @@ class Scheduler(Threaded):
             str, dict[str, dict[str, Message]]
         ] = {}  # {node_id: [message, ...]}
 
+        self.scores = {}  # {node_id: score}
+
     def queue_msg(self, msg: Message):
         node_queue = self.queue.get(msg.node_id, {})
 
@@ -27,15 +29,31 @@ class Scheduler(Threaded):
 
         self.queue[msg.node_id] = node_queue
 
-    def get_next_node(self) -> str:
-        # Chossing node based on the balance / amount
-        return max(
-            self.queue.keys(),
-            key=lambda n: len(self.queue[n]) / self.node.tangle.get_balance(n),
-        )
+        self.update_score(msg.node_id)
+
+    def update_score(self, node_id: str):
+        if node_id not in self.queue:
+            if node_id in self.scores:
+                del self.scores[node_id]
+            return
+
+        now = time.time()
+
+        # Disregarding messages with timestamps in the future
+        valid_msgs = sum(1 for m in self.queue[node_id].values() if m.timestamp <= now)
+
+        if valid_msgs == 0:
+            del self.scores[node_id]
+            return
+
+        score = self.node.tangle.get_balance(node_id) / valid_msgs
+
+        self.scores[node_id] = score
 
     def get_next_message(self) -> Message:
-        msgs = self.queue[self.get_next_node()]
+        node_id = max(self.scores)
+
+        msgs = self.queue[node_id]
 
         # Getting the oldest message
         oldest_msg = sorted(msgs.values(), key=lambda m: m.timestamp)[0]
@@ -45,18 +63,20 @@ class Scheduler(Threaded):
     def process_next_message(self):
         msg = self.get_next_message()
 
-        # Processing the message
-        self.node.add_msg_from_queue(msg)
-
         # Unqueueing the message
         del self.queue[msg.node_id][msg.hash]
 
         if self.queue[msg.node_id] == {}:
             del self.queue[msg.node_id]
 
+        # Processing the message
+        self.node.add_msg_from_queue(msg)
+
+        self.update_score(msg.node_id)
+
     def run(self):
         while not self.terminate_flag.is_set():
-            if self.queue:
+            if self.scores:
                 self.process_next_message()
 
             time.sleep(SCHEDULING_RATE)
